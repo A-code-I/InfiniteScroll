4dc8d1518683d23863cbf7163a039af50a63939d
rajesh appana <appanarajesh363@gmail.com>
2023-09-16 12:56:16 +0530

    infinite scroll trigger & loader functionality

--- script.js
+++ script.js
@@ -2,13 +2,31 @@ const imgContainer=document.getElementById('Image-container');
 const loader=document.getElementById('loader');
 
 let photosArray=[];
+
+let ready=true;
+let imgLoaded=0;
+let totalImages=0;
+
 // Unsplash API
-const count=30;
+const count=5;
 const topic='church';
 const collections='Jesus';
-const api_key='pzUppk1cHNzwwbjdYs7lZo5QjZqh0VXIe_XreLfEI44';
+const api_key='HD9nnO1thKkhfDmKIRRj-dRDjvxs5fHYHodqC6ftgg4';  //slow tes2
+// const api_key='pzUppk1cHNzwwbjdYs7lZo5QjZqh0VXIe_XreLfEI44';
 // const api_key='Ec9_2tLPsKQGvKrZ5Bwb81-jtKpupAwfNCXcqrpl8BU';
 const apiUrl=`https://api.unsplash.com/photos/random?client_id=${api_key}&collections=${collections}&topics=${topic}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded(){
+    imgLoaded++;
+    // console.log(imgLoaded);
+    if (imgLoaded===totalImages){
+        ready=true;
+        loader.hidden=true;
+        // console.log('ready=',ready);
+    }
+}
+
 
 // Helper function to setAttributes on DOM Elements
 function setAttributes(element,attributes){
@@ -18,6 +36,9 @@ function setAttributes(element,attributes){
 }
 // Display photos 
 function displayPhotos(){
+    imgLoaded=0;
+    totalImages=photosArray.length;
+    // console.log("Total Images =",totalImages);
     // Run function for each objects in PhotoArray
     photosArray.forEach((photo) =>{
         // Creation of Anchor element to link to unsplash
@@ -33,6 +54,9 @@ function displayPhotos(){
             alt:photo.alt_description,
             title:photo.alt_description,
         });
+
+        // Event listener to check when each is finished loading 
+        img.addEventListener('load',imageLoaded);
         // Put <img> inside <a> and put both inside image container
         anchorItem.appendChild(img);
         imgContainer.appendChild(anchorItem);      
@@ -52,9 +76,9 @@ async function getPhotos(){
 
 // check to see if scroilling near bottom of page ,load mor photos
 window.addEventListener('scroll', ()=> {
-    if(window.innerHeight+window.scrollY>document.body.offsetHeight-1000){
+    if(window.innerHeight+window.scrollY>document.body.offsetHeight-1000 && ready){
+        ready=false;
         getPhotos();
-        console.log("load more");
     }
 })
 //On Load
